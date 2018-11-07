f15c6e5a657ba44f0636b0caf9660720d5101ec0
unknown <davidgamboa2727@gmail.com>
2018-11-07 14:29:15 -0500

    initial commit

---
+++ src/Maze.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Maze {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter a file name for a maze");
+	}
+
+}
