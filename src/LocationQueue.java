0e3ea1689f745c36bd78d5a8b14bc265a16d5d1a
unknown <davidgamboa2727@gmail.com>
2018-11-27 07:47:23 -0500

    allfiles

---
+++ src/LocationQueue.java
@@ -0,0 +1,25 @@
+
+public class LocationQueue {
+private int x;
+private int y;
+	public LocationQueue(int x , int y) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setX(int x) {
+		this.x=x;
+	}
+	
+	public void setY(int y) {
+		this.y=y;
+	}
+
+
+}
